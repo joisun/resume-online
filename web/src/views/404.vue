047da775c179b73e5c081b9ddcfd13cabdd3147b
jaycethanks <jaycethanks@outlook.com>
2023-08-11 00:48:36 +0800

    feat: 404 page add current route path

--- web/src/views/404.vue
+++ web/src/views/404.vue
@@ -1,7 +1,12 @@
+<script lang="ts" setup>
+import router from '@/router';
+const currentPath = router.currentRoute.value.fullPath
+</script>
 <template>
   <div class="w-full h-screen flex justify-center items-center flex-col">
     <h1 class="text-[48px] font-bold">404</h1>
     <h1 class="text-lg font-bold">NOT FOUND : )</h1>
     <h2 class="mt-5">Have a coffee break ☕️, And have a nice day!</h2>
+    <h3 class="mt-5">Current Path: {{ currentPath }}</h3>
   </div>
 </template>
