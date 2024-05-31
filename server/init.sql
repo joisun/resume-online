2f23dfd57928be3f93cd792d84d1795a5a6c2711
jaycethanks <jaycethanks@outlook.com>
2024-05-31 18:31:20 +0800

    feat: update sql

--- server/init.sql
+++ server/init.sql
@@ -2,7 +2,7 @@ CREATE TABLE online_resume.main (
 	content LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
 	created_time DATETIME NULL,
 	update_time DATETIME NULL
-)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE online_resume.visitors (
 	leave_time DATETIME NULL,
@@ -13,9 +13,4 @@ CREATE TABLE online_resume.visitors (
 	isp varchar(100) NULL,
 	ip varchar(100) NULL,
 	id varchar(100) NOT NULL
-)
-
-
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
